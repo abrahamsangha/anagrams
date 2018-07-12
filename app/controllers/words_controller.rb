5be2589994999ee0040110df74b3e8b78f4568a4
Abraham Sangha <abraham.sangha@cognizant.com>
2018-07-12 16:09:00 -0600

    Adds WordsController with #create action

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,12 @@
 class WordsController < ApplicationController
   def create
-
+    words_params[:words].each { |word| Word.create(word) }
+    render status: :created
   end
+
+  def words_params
+    params.require(:word).permit(words: [])
+  end
+
+
 end
